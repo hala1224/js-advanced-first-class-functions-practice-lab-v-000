3b9f1764cc9d4ee613f6a290f074bf415368161c
Natalie Halawani <hala1224@gmail.com>
2018-10-28 21:40:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ function driversByRevenue(drivers) {
   })
 }
 
+function driversByName(drivers) {
+  return [...drivers].sort(function(a,b){
+    return a.name>b.name
+  })
+}
+
+
 function totalRevenue(drivers) {
   const revenues = [];
   drivers.forEach(function(driver){revenues.push(driver.revenue)})
